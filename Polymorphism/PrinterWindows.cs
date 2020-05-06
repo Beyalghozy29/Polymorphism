0e1bcb944924c2cf0dc6b991639133b05fbbaba9
beyalghozy <muhammad.2903@students.amikom.ac.id>
2020-05-06 21:12:56 +0700

    Polymorphism

---
+++ Polymorphism/PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Pilihan Printer");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Pilihan Printer");
+        }
+    }
+}
